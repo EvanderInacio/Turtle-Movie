f80df40c7012a65de14e87229339a30e0c39d242
Evander Inacio <evander.20112@hotmail.com>
2022-10-03 18:48:10 -0300

    Finished project :tada:

--- src/pages/Persons/Person.jsx
+++ src/pages/Persons/Person.jsx
@@ -33,7 +33,7 @@ export function Person() {
           src={`https://image.tmdb.org/t/p/original${
             person ? person.profile_path : ''
           }`}
-          alt={person.name}
+          alt="Pessoa"
         />
 
         <Info>
